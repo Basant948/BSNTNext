a38da71ce667edb773ceb76d663c548b2fbc1a74
basanti <basant123bsnt@gmail.com>
2026-03-23 19:53:36 +0545

    Add  Register Dtos using fluent validations

---
+++ BSNTNext.Application/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Dtos.Auth
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+        public string? LastName { get; set; }
+    }
+}
---
+++ BSNTNext.Application/Validations/RegisterDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using BSNTNext.Application.Dtos.Auth;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Validations
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6);
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+
+            RuleFor(x => x.LastName)
+                .MaximumLength(50);
+        }
+    }
+}
