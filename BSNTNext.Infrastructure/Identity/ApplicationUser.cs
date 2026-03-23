947b9b378ff52f3f3ad637b01d58599daefe9fcb
basanti <basant123bsnt@gmail.com>
2026-03-23 19:21:43 +0545

    Add Customize identity model

---
+++ BSNTNext.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using BSNTNext.Domain.Base;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Infrastructure.Identity
+{
+    public class ApplicationUser  : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = null!;
+        public string? LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
