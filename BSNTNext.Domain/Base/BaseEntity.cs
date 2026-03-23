fbee8cd43b92ba70cdcb2f7598d3207ccae4db21
basanti <basant123bsnt@gmail.com>
2026-03-23 17:54:05 +0545

    Add Base Entity

---
+++ BSNTNext.Domain/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
