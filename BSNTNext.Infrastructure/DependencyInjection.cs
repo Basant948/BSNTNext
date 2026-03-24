db2851ebf8faf39fd40df33c2bbb7ffedd7619bd
basanti <basant123bsnt@gmail.com>
2026-03-24 12:03:58 +0545

    Add Dependency Injection for AuthServices

---
+++ BSNTNext.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using BSNTNext.Application.Interfaces.Services;
+using BSNTNext.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BSNTNext.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthServices, AuthServices>();
+           
+
+            return services;
+        }
+    }
+}
