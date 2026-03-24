9585a81eaf061f8b44d39c6e615be9637f05de34
basanti <basant123bsnt@gmail.com>
2026-03-24 09:23:32 +0545

    Add generic Result class with success/failure handling

---
+++ BSNTNext.Application/Common/Result.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Common
+{
+    public class Result
+    {
+        public bool Succeeded { get; set; }
+
+        public string? Message { get; set; }
+
+        public List<string> Errors { get; set; } = new();
+
+        public static Result Success(string? message = null)
+        {
+            return new Result
+            {
+                Succeeded = true,
+                Message = message
+            };
+        }
+
+        public static Result Failure(string error)
+        {
+            return new Result
+            {
+                Succeeded = false,
+                Errors = new List<string> { error }
+            };
+        }
+
+        public static Result Failure(IEnumerable<string> errors)
+        {
+            return new Result
+            {
+                Succeeded = false,
+                Errors = errors.ToList()
+            };
+        }
+    }
+}
