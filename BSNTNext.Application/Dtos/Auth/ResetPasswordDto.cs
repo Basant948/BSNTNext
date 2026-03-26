33b3e435c089e879d76e8e714b5fcf5a20a71adf
basanti <basant123bsnt@gmail.com>
2026-03-26 20:36:09 +0545

    Add DTOs for forgot password and reset password requests

---
+++ BSNTNext.Application/Dtos/Auth/ResetPasswordDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BSNTNext.Application.Dtos.Auth
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
