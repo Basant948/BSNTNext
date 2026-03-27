506d846529c859f857d394f019fa0f1bace5f7a3
basanti <basant123bsnt@gmail.com>
2026-03-27 07:56:14 +0545

    Add Dto for changepassword using fluent validations

---
+++ BSNTNext.Application/Dtos/Auth/ChangePasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Dtos.Auth
+{
+    public class ChangePasswordDto
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ BSNTNext.Application/Validations/ChangePasswordDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using BSNTNext.Application.Dtos.Auth;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Validations
+{
+    public class ChangePasswordDtoValidator : AbstractValidator<ChangePasswordDto>
+    {
+        public ChangePasswordDtoValidator() 
+        {
+            RuleFor(x => x.OldPassword)
+                .NotEmpty()
+                .MinimumLength(6);
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .MinimumLength(6);
+
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty()
+                .MinimumLength(6)
+                .Equal(x => x.NewPassword).WithMessage("New password and confirmation do not match.");
+        }
+    }
+}
