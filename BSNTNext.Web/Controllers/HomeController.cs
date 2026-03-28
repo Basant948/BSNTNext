7b1d73811d2480f167a55c758ddbcc01fc268751
basanti <basant123bsnt@gmail.com>
2026-03-28 18:09:32 +0545

    Reapply "Run Migration"
    This reverts commit 908a8c78cec53f23d0b57dfb4dce66e7f71b4e4a.

--- BSNTNext.Web/Controllers/HomeController.cs
+++ BSNTNext.Web/Controllers/HomeController.cs
@@ -1,14 +1,55 @@
-﻿using BSNTNext.Web.Models;
+﻿using BSNTNext.Domain.Entity;
+using BSNTNext.Infrastructure.Data;
+using BSNTNext.Infrastructure.Identity;
+using BSNTNext.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BSNTNext.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
-            return View();
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return RedirectToAction("Login", "Account");
+
+            var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+
+            List<AppModule> modules;
+
+            if (isAdmin)
+            {
+                modules = await _db.AppModules  // ✅ fixed
+                    .Where(m => m.IsActive)
+                    .OrderBy(m => m.SortOrder)
+                    .ToListAsync();
+            }
+            else
+            {
+                modules = await _db.UserModuleAccesses
+                    .Where(a => a.UserId == user.Id)
+                    .Include(a => a.Module)
+                    .Where(a => a.Module.IsActive)
+                    .Select(a => a.Module)
+                    .OrderBy(m => m.SortOrder)
+                    .ToListAsync();
+            }
+
+            ViewBag.UserFullName = $"{user.FirstName} {user.LastName}".Trim();  // ✅ fixed
+            ViewBag.IsAdmin = isAdmin;
+            return View(modules);
         }
 
         public IActionResult Privacy()
