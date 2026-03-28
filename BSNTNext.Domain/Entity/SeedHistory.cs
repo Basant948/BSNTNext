699e935a3e714ef486a6899786aa0ea2a782d936
basanti <basant123bsnt@gmail.com>
2026-03-28 08:30:02 +0545

    Add seedhistory model and migration

---
+++ BSNTNext.Domain/Entity/SeedHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Domain.Entity
+{
+    public class SeedHistory
+    {
+        public int Id { get; set; }
+        public string SeedKey { get; set; } = null!;
+        public DateTime AppliedAt { get; set; }
+        public string? Notes { get; set; }
+    }
+}
