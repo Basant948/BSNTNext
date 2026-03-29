122b8d574eecc3ca167f356fa19ef7d011527b87
basanti <basant123bsnt@gmail.com>
2026-03-29 09:42:39 +0545

    Add IHomeService interface and DashboardResult DTO

---
+++ BSNTNext.Application/Dtos/Home/DashboardResult.cs
@@ -0,0 +1,13 @@
+﻿using BSNTNext.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Dtos.Home
+{
+    public class DashboardResult
+    {
+        public string UserFullName { get; set; } = string.Empty;
+        public List<AppModule> Modules { get; set; } = new();
+    }
+}
---
+++ BSNTNext.Application/Interfaces/Services/IHomeService.cs
@@ -0,0 +1,12 @@
+﻿using BSNTNext.Application.Dtos.Home;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Interfaces.Services
+{
+    public interface IHomeService
+    {
+        Task<DashboardResult?> GetDashboardAsync(string userId);
+    }
+}
