676d1fc60bb06992a25ec9e7bd5bcfee3cd55094
basanti <basant123bsnt@gmail.com>
2026-03-30 21:07:22 +0545

    Add role management UI and module layout views

---
+++ BSNTNext.Application/Dtos/Nav/NavItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSNTNext.Application.Dtos.Nav
+{
+    public class NavItemDto
+    {
+        public string Key { get; set; } = "";
+        public string Label { get; set; } = "";
+        public string Icon { get; set; } = "";
+        public string Url { get; set; } = "";
+        public string Group { get; set; } = "";
+    }
+}
